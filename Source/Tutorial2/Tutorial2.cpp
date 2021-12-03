c9c15949cba42813fa782d7370772bd3ccce165b
Zardrian70 <russh70285@gmail.com>
2021-12-02 17:04:24 -0800

    Submitted Tutorial
    Initial Commit

---
+++ Source/Tutorial2/Tutorial2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tutorial2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tutorial2, "Tutorial2" );
